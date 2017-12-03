fadc7645048a256e940a1eac6d7434fe0ae0e760
Unknown <thanosroidis@hotmail.com>
2017-12-03 03:21:36 +0100

    Naive Bayes training
    Training finished, only sampling left

--- Assignment 3/a3_simple_template.py
+++ Assignment 3/a3_simple_template.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
-
+import time
+from datetime import datetime
 
 def load_mnist_images(binarize=True):
     """
@@ -27,29 +28,79 @@ class NaiveBayesModel(object):
         :param b_init: A (n_categories, ) vector where b[i] represents log p(Z[i]=1), or None to fill with zeros
         :param c_init: A (n_dim, ) vector where b[j] represents log p(X[j]=1), or None to fill with zeros
         """
-        raise NotImplementedError()
+
+        self._n_categories = w_init.shape[0]
+        self._n_dim = w_init.shape[1]
+
+        if b_init is None:
+            b_init =  np.zeros(self._n_categories, )
+        if c_init is None:
+            c_init = np.zeros(self._n_dim, )
+
+        self.W = tf.get_variable('w', shape=w_init.shape, initializer=tf.constant_initializer(w_init))
+
+        self.b = tf.get_variable('b', shape=b_init.shape, initializer=tf.constant_initializer(b_init))
+
+        self.c = tf.get_variable('c', shape=c_init.shape, initializer=tf.constant_initializer(c_init))
+
+
 
     def log_p_x_given_z(self, x, z):
         """
         :param x: An (n_samples, n_dims) tensor
         :param z: An (n_samples, n_labels) tensor of integer class labels
         :return: An (n_samples, n_labels) tensor  p_x_given_z where result[i, j] indicates p(X=x[i] | Z=z[j])
+
         """
-        raise NotImplementedError()
+
+        #copy 'x' tensor 'n_categories' times along on the 3rd dimension [batch_size, n_dim]
+        x_bc = tf.tile(tf.expand_dims(x, -1), [1, 1, self._n_categories])
+
+        theta = tf.nn.sigmoid(tf.add(self.W, self.c))
+        theta = tf.transpose(theta)
+
+        log_probs = tf.log( x_bc * theta + (1-x_bc) * (1-theta))
+
+        log_probs = tf.reduce_sum(log_probs, axis = 1)
+
+
+        self.x_bc = x_bc
+        self.theta = theta
+
+
+        return log_probs
+
+
 
     def log_p_x(self, x):
         """
         :param x: A (n_samples, n_dim) array of data points
         :return: A (n_samples, ) array of log-probabilities assigned to each point
         """
-        raise NotImplementedError()
+
+        log_p_x_given_z = self.log_p_x_given_z(x, None)
+
+        log_p_z = tf.nn.log_softmax(self.b)
+
+        t = log_p_z + log_p_x_given_z
+
+        return tf.reduce_logsumexp(t, axis = 1)
+
+
 
     def sample(self, n_samples):
         """
         :param n_samples: Generate N samples from your model
         :return: A (n_samples, n_dim) array where n_dim is the dimenionality of your input
         """
-        raise NotImplementedError()
+
+        cat_dist = tf.distributions.Categorical(logits=tf.nn.log_softmax(self.b))
+        z_samples = cat_dist.sample(n_samples)
+
+        bern_dist = tf.distributions.Bernoulli(probs = self.theta)
+        samples = bern_dist.sample(n_samples)
+
+        return samples, z_samples
 
 
 def train_simple_generative_model_on_mnist(n_categories=20, initial_mag = 0.01, optimizer='rmsprop', learning_rate=.01, n_epochs=20, test_every=100,
@@ -67,24 +118,86 @@ def train_simple_generative_model_on_mnist(n_categories=20, initial_mag = 0.01, 
     :param plot_n_samples: Number of samples to plot
     """
 
+    # minibatch_size = 2
+    # n_categories = 3
+    # Z = tf.placeholder(tf.float32, [None, n_categories])
+
     # Get Data
     x_train, x_test = load_mnist_images(binarize=True)
-    train_iterator = tf.data.Dataset.from_tensor_slices(x_train).repeat().batch(minibatch_size).make_initializable_iterator()
+    train_iterator = tf.contrib.data.Dataset.from_tensor_slices(x_train).repeat().batch(minibatch_size).make_initializable_iterator()
     n_samples, n_dims = x_train.shape
     x_minibatch = train_iterator.get_next()  # Get symbolic data, target tensors
 
+    print(n_samples, n_dims)
+    print(type(x_minibatch))
+    print(x_minibatch)
+
     # Build the model
-    raise NotImplementedError('Insert Model Building Code')
+    w_init = np.random.normal(scale = initial_mag, size = (n_categories, n_dims))
+    b_init = np.random.normal(scale = initial_mag, size = (n_categories, ))
+    c_init = np.zeros(n_dims, )
+
+    model = NaiveBayesModel(w_init, b_init, c_init)
+
+    loss = -tf.reduce_mean(model.log_p_x(x_minibatch))
+
+    train_step = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(loss)
+
+    # samples, z_samples = model.sample(plot_n_samples)
+
+    log_path = './summaries/NB_' + time.strftime("%Y%m%d-%H%M")
+    if not tf.gfile.Exists(log_path):
+        tf.gfile.MakeDirs(log_path)
+    writer = tf.summary.FileWriter(log_path)
+
+    images_placeholder = tf.placeholder("float", [None,28,28,1], name='ims')
+    im_summary = tf.summary.image('params',images_placeholder, max_outputs = 100)
+
 
     with tf.Session() as sess:
         sess.run(train_iterator.initializer)
         sess.run(tf.global_variables_initializer())
-        n_steps = (n_epochs * n_samples)/minibatch_size
-        for i in xrange(n_steps):
-            if i%test_every==0:
-                raise NotImplementedError('INSERT CODE TO RUN TEST AND RECORD LOG-PROB PERIODICALLY')
+        n_steps =  int((n_epochs * n_samples)/minibatch_size)
 
-            raise NotImplementedError('COMPUTE TRAINING UPDATES HERE')
+        for i in range(n_steps):
+            # Only for time measurement of step through network
+            t1 = time.time()
+
+
+            [_, loss_value] = sess.run([train_step, loss])
+
+
+            # Only for time measurement of step through network
+            t2 = time.time()
+            examples_per_second = minibatch_size / float(t2 - t1)
+
+
+            if i%test_every==0:
+
+                print("[{}] Train Step {:04d}/{:04d}, "
+                      "Examples/Sec = {:.2f}, Loss = {}".format(
+                    datetime.now().strftime("%Y-%m-%d %H:%M"), i,
+                    n_steps,  examples_per_second,
+                    loss_value))
+
+
+                theta_val = sess.run(model.theta)
+                theta_val = theta_val.reshape((28,28,20))
+                theta_val = np.transpose(theta_val, [2,0,1])
+                theta_val = np.expand_dims(theta_val,axis = -1)
+
+                # [samples_val, z_samples_val] = sess.run([samples,z_samples])
+
+                # samples_val = samples_val[:, :, z_samples_val]
+
+                # print(samples_val.shape)
+                summary_result = sess.run(im_summary, feed_dict={images_placeholder:theta_val})
+
+                writer.add_summary(summary_result, global_step=i)
+
+                # raise NotImplementedError('INSERT CODE TO RUN TEST AND RECORD LOG-PROB PERIODICALLY')
+
+            # raise NotImplementedError('COMPUTE TRAINING UPDATES HERE')
 
 
 if __name__ == '__main__':
